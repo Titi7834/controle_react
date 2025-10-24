46a674defe184d27fd482898366099fd98d83060
Titi7834 <tiago.dc78@gmail.com>
2025-10-24 09:31:10 +0200

    Commit du fichier data pour exploiter l'API

---
+++ src/data/data.tsx
@@ -0,0 +1,26 @@
+import User from '../model/user';
+
+async function getProducts() {
+    const fakeAPI = fetch('https://dummyjson.com/users')
+        .then(res => res.json())
+        .then(data => data.users);
+
+    return new Promise<User[]>(async (resolve) => {
+        const productsData = await fakeAPI;
+        const products: User[] = productsData.map((util: any) => new User(
+            util.id,
+            util.firstname,
+            util.lastname,
+            util.image,
+            util.email,
+            util.age,
+            util.company,
+            util.address.city,
+            util.bloodgroup,
+            util.height
+        ));
+        resolve(products);
+    });
+}
+
+export { getProducts };
